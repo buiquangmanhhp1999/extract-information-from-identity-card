1fc393c68c3916d127c062a1cc7c8d52dc5dea98
buiquangmanhhp1999 <48142689+buiquangmanhhp1999@users.noreply.github.com>
2020-10-04 18:20:50 +0700

    Add files via upload

---
+++ complete/serve_model.py
@@ -0,0 +1,32 @@
+from io import BytesIO
+import numpy as np
+from PIL import Image
+from merged_model import CompletedModel
+
+model = None
+
+
+def load_model():
+    print("Model loading.....")
+    model = CompletedModel()
+    print("Model loaded")
+
+    return model
+
+
+def predict(image: Image.Image):
+    global model
+    if model is None:
+        model = load_model()
+
+    img = np.asarray(image)
+    result = model.predict(img)
+    print(result)
+
+    return result
+
+
+def read_image_file(file) -> Image.Image:
+    image = Image.open(BytesIO(file))
+
+    return image
